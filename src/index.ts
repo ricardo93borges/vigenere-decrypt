ef05895263fbaf145f20d264587fac968b72ca97
ricardo93borges <ricardo93borges@gmail.com>
2020-03-31 22:41:58 -0300

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,18 @@
 import FileManager from './FileManager'
-import calculateIC from './ic'
-import { alphabet } from './alphabet'
+import Vigenere from './vigenere'
 
-interface PossibleLengths {
-  [key: number]: number;
+function run() {
+  const fileManager = new FileManager(`${__dirname}/cipher.txt`)
+  let cipherText = fileManager.read()
+  cipherText = cipherText.toLowerCase().split(' ').join('').trim()
+
+
+  const vigenere = new Vigenere(cipherText)
+  const key = vigenere.findKey()
+  const plainText = vigenere.decrypt(key)
+
+  console.log('> Key', key)
+  console.log('> Plain text', plainText)
 }
 
-function average(arr: number[]): number {
-  let sum = 0
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i]
-  }
-  return sum / arr.length
-}
-
-function sort(possibleLengths: PossibleLengths): any[][] {
-  const sortable = []
-  for (const length in possibleLengths)
-    sortable.push(possibleLengths[length])
-
-  const sorted = Object.entries(possibleLengths).sort((a, b) => a[1] < b[1] ? 1 : -1)
-  return sorted
-}
-
-function findKeyLength(cipherText: string): number {
-  const possibleLengths: PossibleLengths = {}
-
-  for (let keylength = 2; keylength < 15; keylength++) {
-    const ICs = []
-
-    for (let j = 0; j < keylength; j++) {
-      const letters = []
-      let currentIndex = j
-
-      letters.push(cipherText[currentIndex])
-
-      for (let i = j; i < cipherText.length / keylength; i++) {
-        currentIndex += keylength
-        letters.push(cipherText[currentIndex])
-      }
-
-      ICs.push(calculateIC(letters.join('')))
-    }
-
-    possibleLengths[keylength] = average(ICs)
-  }
-
-  const sorted = sort(possibleLengths)
-  return parseInt(sorted[0][0], 10)
-}
-
-function frequencyAnalysis(substring: string): { [key: string]: number } {
-  const ocurrencies: { [key: string]: number } = {}
-
-  for (let j = 0; j < alphabet.length; j++) {
-    ocurrencies[alphabet[j]] = 0
-  }
-
-  for (let i = 0; i < substring.length; i++) {
-    ocurrencies[substring[i]]++
-  }
-
-  for (let j = 0; j < alphabet.length; j++) {
-    ocurrencies[alphabet[j]] = ocurrencies[alphabet[j]] / substring.length
-  }
-
-  return ocurrencies
-}
-
-const fileManager = new FileManager(`${__dirname}/cipher.txt`)
-let cipherText = fileManager.read()
-cipherText = cipherText.toLowerCase().split(' ').join('').trim()
-
-// find key length
-const keylength = findKeyLength(cipherText)
-console.log('keylength', keylength)
-// frequency analysis
-let key = ''
-let i = 0
-while (i < keylength) {
-  let currentIndex = i
-  const letters = []
-  letters.push(cipherText[currentIndex])
-
-  while (currentIndex + keylength < cipherText.length) {
-    currentIndex = currentIndex + keylength
-    letters.push(cipherText[currentIndex])
-  }
-
-  const substring = letters.join('')
-  const frequencies = frequencyAnalysis(substring)
-
-  const sorted = Object.entries(frequencies).sort((a, b) => a[1] < b[1] ? 1 : -1)
-  //console.log(sorted)
-  key = key.concat(sorted[0][0])
-  i++
-}
-
-
-console.log(key)
+run()
--- src/vigenere.ts
+++ src/vigenere.ts
@@ -1,88 +1,192 @@
+import calculateIC from './ic'
+import { alphabet } from './alphabet'
+
+interface PossibleLengths {
+  [key: number]: number;
+}
 
 interface VigenereTable {
   [key: string]: string;
 }
 
-const table: VigenereTable = {
-  'a': 'abcdefghijklmnopqrstuvwxyz',
-  'b': 'bcdefghijklmnopqrstuvwxyza',
-  'c': 'cdefghijklmnopqrstuvwxyzab',
-  'd': 'defghijklmnopqrstuvwxyzabc',
-  'e': 'efghijklmnopqrstuvwxyzabcd',
-  'f': 'fghijklmnopqrstuvwxyzabcde',
-  'g': 'ghijklmnopqrstuvwxyzabcdef',
-  'h': 'hijklmnopqrstuvwxyzabcdefg',
-  'i': 'ijklmnopqrstuvwxyzabcdefgh',
-  'j': 'jklmnopqrstuvwxyzabcdefghi',
-  'k': 'klmnopqrstuvwxyzabcdefghij',
-  'l': 'lmnopqrstuvwxyzabcdefghijk',
-  'm': 'mnopqrstuvwxyzabcdefghijkl',
-  'n': 'nopqrstuvwxyzabcdefghijklm',
-  'o': 'opqrstuvwxyzabcdefghijklmn',
-  'p': 'pqrstuvwxyzabcdefghijklmno',
-  'q': 'qrstuvwxyzabcdefghijklmnop',
-  'r': 'rstuvwxyzabcdefghijklmnopq',
-  's': 'stuvwxyzabcdefghijklmnopqr',
-  't': 'tuvwxyzabcdefghijklmnopqrs',
-  'u': 'uvwxyzabcdefghijklmnopqrst',
-  'v': 'vwxyzabcdefghijklmnopqrstu',
-  'w': 'wxyzabcdefghijklmnopqrstuv',
-  'x': 'xyzabcdefghijklmnopqrstuvw',
-  'y': 'yzabcdefghijklmnopqrstuvwx',
-  'z': 'zabcdefghijklmnopqrstuvwxy'
-}
-
-export function encrypt(plainText: string, key: string): string {
-  plainText = plainText.split(' ').join('').trim().toLowerCase()
-
-  let cipher = ''
-  let keyCharIndex = 0
-  for (let i = 0; i < plainText.length; i++) {
-    if (keyCharIndex > key.length - 1) {
-      keyCharIndex = 0
-    }
-
-    const lineIndex = key[keyCharIndex]
-    const plainTextChar = plainText[i]
-    const columnIndex = Object.keys(table).findIndex(k => k === plainTextChar)
-
-    let char = plainTextChar
-    if (columnIndex > -1) {
-      char = table[lineIndex][columnIndex]
-    }
-
-    cipher = cipher.concat(char)
-    keyCharIndex++
+export default class Vigenere {
+  private cipher: string
+  private table: VigenereTable = {
+    'a': 'abcdefghijklmnopqrstuvwxyz',
+    'b': 'bcdefghijklmnopqrstuvwxyza',
+    'c': 'cdefghijklmnopqrstuvwxyzab',
+    'd': 'defghijklmnopqrstuvwxyzabc',
+    'e': 'efghijklmnopqrstuvwxyzabcd',
+    'f': 'fghijklmnopqrstuvwxyzabcde',
+    'g': 'ghijklmnopqrstuvwxyzabcdef',
+    'h': 'hijklmnopqrstuvwxyzabcdefg',
+    'i': 'ijklmnopqrstuvwxyzabcdefgh',
+    'j': 'jklmnopqrstuvwxyzabcdefghi',
+    'k': 'klmnopqrstuvwxyzabcdefghij',
+    'l': 'lmnopqrstuvwxyzabcdefghijk',
+    'm': 'mnopqrstuvwxyzabcdefghijkl',
+    'n': 'nopqrstuvwxyzabcdefghijklm',
+    'o': 'opqrstuvwxyzabcdefghijklmn',
+    'p': 'pqrstuvwxyzabcdefghijklmno',
+    'q': 'qrstuvwxyzabcdefghijklmnop',
+    'r': 'rstuvwxyzabcdefghijklmnopq',
+    's': 'stuvwxyzabcdefghijklmnopqr',
+    't': 'tuvwxyzabcdefghijklmnopqrs',
+    'u': 'uvwxyzabcdefghijklmnopqrst',
+    'v': 'vwxyzabcdefghijklmnopqrstu',
+    'w': 'wxyzabcdefghijklmnopqrstuv',
+    'x': 'xyzabcdefghijklmnopqrstuvw',
+    'y': 'yzabcdefghijklmnopqrstuvwx',
+    'z': 'zabcdefghijklmnopqrstuvwxy'
   }
 
-  return cipher
-}
+  constructor(cipher: string) {
+    this.cipher = cipher
+  }
 
-export function decrypt(cipher: string, key: string): string {
-  let text = ''
-  let keyCharIndex = 0
-  for (let i = 0; i < cipher.length; i++) {
-    if (keyCharIndex > key.length - 1) {
-      keyCharIndex = 0
+  encrypt(plainText: string, key: string): string {
+    plainText = plainText.split(' ').join('').trim().toLowerCase()
+
+    let cipher = ''
+    let keyCharIndex = 0
+    for (let i = 0; i < plainText.length; i++) {
+      if (keyCharIndex > key.length - 1) {
+        keyCharIndex = 0
+      }
+
+      const lineIndex = key[keyCharIndex]
+      const plainTextChar = plainText[i]
+      const columnIndex = Object.keys(this.table).findIndex(k => k === plainTextChar)
+
+      let char = plainTextChar
+      if (columnIndex > -1) {
+        char = this.table[lineIndex][columnIndex]
+      }
+
+      cipher = cipher.concat(char)
+      keyCharIndex++
     }
 
-    const lineIndex = key[keyCharIndex]
-    const cipherChar = cipher[i]
+    return cipher
+  }
 
-    const line = table[lineIndex]
-    const columnIndex = line.split('').findIndex(k => k === cipherChar)
+  decrypt(key: string): string {
+    let text = ''
+    let keyCharIndex = 0
+    for (let i = 0; i < this.cipher.length; i++) {
+      if (keyCharIndex > key.length - 1) {
+        keyCharIndex = 0
+      }
 
-    let char: string | undefined = cipherChar
-    if (columnIndex > -1) {
-      char = Object.keys(table).find((letter, index) => {
-        if (index === columnIndex) {
-          return letter
+      const lineIndex = key[keyCharIndex]
+      const cipherChar = this.cipher[i]
+
+      const line = this.table[lineIndex]
+      const columnIndex = line.split('').findIndex(k => k === cipherChar)
+
+      let char: string | undefined = cipherChar
+      if (columnIndex > -1) {
+        char = Object.keys(this.table).find((letter, index) => {
+          if (index === columnIndex) {
+            return letter
+          }
+        })
+      }
+
+      text = char ? text.concat(char) : text.concat(this.cipher[i])
+      keyCharIndex++
+    }
+    return text
+  }
+
+  average(arr: number[]): number {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i]
+    }
+    return sum / arr.length
+  }
+
+  sort(possibleLengths: PossibleLengths): any[][] {
+    const sortable = []
+    for (const length in possibleLengths)
+      sortable.push(possibleLengths[length])
+
+    const sorted = Object.entries(possibleLengths).sort((a, b) => a[1] < b[1] ? 1 : -1)
+    return sorted
+  }
+
+  findKeyLength(cipherText: string): number {
+    const possibleLengths: PossibleLengths = {}
+
+    for (let keylength = 2; keylength < 15; keylength++) {
+      const ICs = []
+
+      for (let j = 0; j < keylength; j++) {
+        const letters = []
+        let currentIndex = j
+
+        letters.push(cipherText[currentIndex])
+
+        for (let i = j; i < cipherText.length / keylength; i++) {
+          currentIndex += keylength
+          letters.push(cipherText[currentIndex])
         }
-      })
+
+        ICs.push(calculateIC(letters.join('')))
+      }
+
+      possibleLengths[keylength] = this.average(ICs)
     }
 
-    text = char ? text.concat(char) : text.concat(cipher[i])
-    keyCharIndex++
+    const sorted = this.sort(possibleLengths)
+    return parseInt(sorted[0][0], 10)
   }
-  return text
+
+  frequencyAnalysis(substring: string): { [key: string]: number } {
+    const ocurrencies: { [key: string]: number } = {}
+
+    for (let j = 0; j < alphabet.length; j++) {
+      ocurrencies[alphabet[j]] = 0
+    }
+
+    for (let i = 0; i < substring.length; i++) {
+      ocurrencies[substring[i]]++
+    }
+
+    for (let j = 0; j < alphabet.length; j++) {
+      ocurrencies[alphabet[j]] = ocurrencies[alphabet[j]] / substring.length
+    }
+
+    return ocurrencies
+  }
+
+  findKey(): string {
+    const keylength = this.findKeyLength(this.cipher)
+    console.log('keylength', keylength)
+
+    // frequency analysis
+    let key = ''
+    let i = 0
+    while (i < keylength) {
+      let currentIndex = i
+      const letters = []
+      letters.push(this.cipher[currentIndex])
+
+      while (currentIndex + keylength < this.cipher.length) {
+        currentIndex = currentIndex + keylength
+        letters.push(this.cipher[currentIndex])
+      }
+
+      const substring = letters.join('')
+      const frequencies = this.frequencyAnalysis(substring)
+
+      const sorted = Object.entries(frequencies).sort((a, b) => a[1] < b[1] ? 1 : -1)
+      key = key.concat(sorted[0][0])
+      i++
+    }
+
+    return key
+  }
+
 }
